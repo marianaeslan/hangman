74d364e063a26aa4488d1a9ec6de55da8164ef66
marianaeslan <mariana.eslan@gmail.com>
2024-07-29 15:02:05 -0400

    input letter guess and checking secret word

--- hangman.py
+++ hangman.py
@@ -11,3 +11,18 @@ print(f"\t\t\tAttempts: {attempts}")
 print("-------------------------")
 print("_ " * len(secret_word))
 print("\n")
+
+#input letter guess and checking if it exists in the secret word
+while attempts > 0:
+    letter_guess = input("\nInsert a letter: ").lower()
+    print("\n")
+    if letter_guess in guessed:
+        print("You already guessed this letter, try another one")
+    if letter_guess in secret_word:
+        guessed.append(letter_guess)
+    else:
+        attempts -= 1
+        print("-------------------------")
+        print("Wrong guess... Try again")
+        print(f"\t\t\tAttempts: {attempts}")
+        print("-------------------------")
